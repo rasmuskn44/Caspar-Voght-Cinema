7258cae49ac9260dc9acf00389703539e4f753ac
rasmuskn44 <rasmus.knoop@gmx.de>
2026-01-29 12:28:29 +0100

    Generate project using spring boot

---
+++ spring-boot/src/main/java/com/RaLe/spring_boot/CasparVoghtCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.RaLe.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasparVoghtCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasparVoghtCinemaApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/com/RaLe/spring_boot/CasparVoghtCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RaLe.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasparVoghtCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
